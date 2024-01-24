848a2a99d99721e3e1198e01ee402fbf6f8fbeaa
Yared Shisema <yaredshi3@gmail.com>
2024-01-24 05:41:48 -0800

    commit redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,43 +1,38 @@
 #!/usr/bin/env python3
 '''A module with tools for request caching and tracking.
 '''
+import redis
 import requests
-import time
 from functools import wraps
+from typing import Callable
 
-def cache_with_expiry(expiry_time):
-    def decorator(func):
-        cache = {}
 
-        @wraps(func)
-        def wrapper(url):
-            if url in cache and time.time() - cache[url]["timestamp"] < expiry_time:
-                cache[url]["count"] += 1
-                return cache[url]["content"]
-            else:
-                content = func(url)
-                cache[url] = {
-                    "content": content,
-                    "count": 1,
-                    "timestamp": time.time()
-                }
-                return content
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
-        return wrapper
 
-    return decorator
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
-@cache_with_expiry(10)
-def get_page(url):
-    response = requests.get(url)
-    return response.text
 
-# Test the get_page function
-url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"
-print(get_page(url))  # This will take 5 seconds to respond
-print(get_page(url))  # This will retrieve the cached result
-
-# Wait for 10 seconds
-time.sleep(10)
-
-print(get_page(url))  # This will fetch the page again since the cache has expired
+@data_cacher
+def get_page(url: str) -> str:
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
